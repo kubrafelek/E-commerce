821f806441c885ab235227dab195924def6e4f9a
kubrafelek <kbr.flk@hotmail.com>
2021-10-25 17:58:39 +0300

    Initial commit

---
+++ src/test/java/kubrafelek/ekmekcini/ecommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package kubrafelek.ekmekcini.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
